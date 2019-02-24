0bfcf1ac212b3912fa0e9b8aa187d5c38f6f2a87
Aayush Gupta <aayu.gupta.96@gmail.com>
2019-02-24 17:04:17 +0530

    Add glm submodule

--- premake5.lua
+++ premake5.lua
@@ -16,6 +16,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "Brute/vendor/GLFW/include"
 IncludeDir["Glad"] = "Brute/vendor/Glad/include"
 IncludeDir["ImGui"] = "Brute/vendor/imgui"
+IncludeDir["glm"] = "Brute/vendor/glm"
 
 include "Brute/vendor/GLFW"
 include "Brute/vendor/Glad"
@@ -38,7 +39,9 @@ project "Brute"
 	files
 	{
 		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl"
 	}
 
 	includedirs
@@ -47,7 +50,8 @@ project "Brute"
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}"
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
 	}
 
 	links
@@ -109,7 +113,8 @@ project "Sandbox"
 	includedirs
 	{
 		"Brute/vendor/spdlog/include",
-		"Brute/src"
+		"Brute/src",
+		"%{IncludeDir.glm}"
 	}
 
 	links
