d001d73ed3bb4db3fa35a0ebaa373d4671ded6bb
Aayush Gupta <aayu.gupta.96@gmail.com>
2019-12-22 15:58:16 +0530

    Render our first triangle!!
    Signed-off-by: Aayush Gupta <aayu.gupta.96@gmail.com>

--- Brute/src/Platform/OpenGL/OpenGLContext.cpp
+++ Brute/src/Platform/OpenGL/OpenGLContext.cpp
@@ -18,6 +18,11 @@ namespace Brute {
 		glfwMakeContextCurrent(m_windowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		BT_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		BT_CORE_INFO("OpenGL Info");
+		BT_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		BT_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		BT_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
