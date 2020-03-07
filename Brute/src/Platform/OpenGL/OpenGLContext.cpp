2e2b5494fac142a2fe552df473923ce6ac94cdb8
Aayush Gupta <aayu.gupta.96@gmail.com>
2020-03-07 10:34:51 +0530

    OpenGL version check
    Signed-off-by: Aayush Gupta <aayu.gupta.96@gmail.com>

--- Brute/src/Platform/OpenGL/OpenGLContext.cpp
+++ Brute/src/Platform/OpenGL/OpenGLContext.cpp
@@ -23,6 +23,14 @@ namespace Brute {
 		BT_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
 		BT_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
 		BT_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
+
+	#ifdef BT_ENABLE_ASSERTS
+		int majorVersion, minorVersion;
+		glGetIntegerv(GL_MAJOR_VERSION, &majorVersion);
+		glGetIntegerv(GL_MINOR_VERSION, &minorVersion);
+
+		BT_CORE_ASSERT(majorVersion > 4 || (majorVersion == 4 && minorVersion >= 5), "Required atleast OpenGL version 4.5!")
+	#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
