59d3c609226ae3b8d305a696ffa7d15355d88db8
Aayush Gupta <aayu.gupta.96@gmail.com>
2020-03-07 10:34:30 +0530

    fixed camera movement after rotation
    Signed-off-by: Aayush Gupta <aayu.gupta.96@gmail.com>

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -11,7 +11,7 @@ class ExampleLayer : public Brute::Layer
 {
 public:
 	ExampleLayer()
-		: Layer("Example"), m_CameraController(1280.0f / 720.0f)
+		: Layer("Example"), m_CameraController(1280.0f / 720.0f, true)
 	{
 		// Vertex Array
 		// Vertex Buffer
